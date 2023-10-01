cea3b78301cfffb6c99ae17824d7b5d0505d1abb
osparking <jbpark03@gmail.com>
2023-10-01 10:50:53 +0900

    login view and get action method for it

---
+++ src/main/java/sq_ch09_ex3/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package sq_ch09_ex3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/")
+	public String loginForm() {
+		return "login.html";
+	}
+}
