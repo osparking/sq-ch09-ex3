809ab1cab75374e4d1217a277fed3658233d6eed
osparking <jbpark03@gmail.com>
2023-10-01 10:29:32 +0900

    application-scope exercise - 3 depend

---
+++ src/main/java/sq_ch09_ex3/SqCh09Ex3Application.java
@@ -0,0 +1,13 @@
+package sq_ch09_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh09Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh09Ex3Application.class, args);
+	}
+
+}
---
+++ src/test/java/sq_ch09_ex3/SqCh09Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch09_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh09Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
