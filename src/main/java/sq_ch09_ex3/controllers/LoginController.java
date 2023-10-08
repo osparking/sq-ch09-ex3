f59d1b6cc6926bd5a78c936d3cbcd2c578c8cbfe
osparking <jbpark03@gmail.com>
2023-10-08 23:09:59 +0900

    for logout, clear username session attr'

--- src/main/java/sq_ch09_ex3/controllers/LoginController.java
+++ src/main/java/sq_ch09_ex3/controllers/LoginController.java
@@ -1,7 +1,5 @@
 package sq_ch09_ex3.controllers;
 
-import java.util.logging.Logger;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +7,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import space.jbpark.utility.MyUtil;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class LoginController {
 	
-	private final Logger logger = MyUtil.getLogger(LoginController.class.getName());
 	private final LoginProcessor loginProcessor;
-	
-	public LoginController(LoginProcessor loginProcessor) {
-		super();
-		this.loginProcessor = loginProcessor;
-		logger.info("로그인 제어기 생성");
-	}
+	private final LoginUser loginUser;
 
 	@GetMapping("/")
 	public String loginForm() {
 		return "login.html";
+	}
+	
+	@GetMapping("/logout")
+	public String processLogout(RedirectAttributes redAttrs) {
+		redAttrs.addFlashAttribute("message", 
+				loginUser.getUsername() + "님이 로그아웃하였습니다.");
+		loginUser.setUsername(null);
+		return "redirect:/";
 	}
 	
 	@PostMapping("/")
@@ -34,7 +35,7 @@ public class LoginController {
 		
 		if (loginProcessor.processLogin(username, password)) {
 			redAttr.addFlashAttribute("message", "로그인 성공");
-			return "redirect:/";
+			return "redirect:/main";
 		} else {
 			page.addAttribute("message", "로그인 실패");
 			return "login.html";
