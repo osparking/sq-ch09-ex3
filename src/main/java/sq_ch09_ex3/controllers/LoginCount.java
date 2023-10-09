87023cf822f81783abf0758a9f61060345e2f08e
osparking <jbpark03@gmail.com>
2023-10-10 06:49:27 +0900

    login count is for this application

---
+++ src/main/java/sq_ch09_ex3/controllers/LoginCount.java
@@ -0,0 +1,17 @@
+package sq_ch09_ex3.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import lombok.Data;
+
+@Component
+@ApplicationScope
+@Data
+public class LoginCount {
+	private long loginCount = 0;
+	public long increase() {
+		return ++loginCount;
+	}
+
+}
